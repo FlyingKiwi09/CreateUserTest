29675732abfda2acee5ff20ae7a0a951806bf749
FlyingKiwi09 <102623183+FlyingKiwi09@users.noreply.github.com>
2023-03-01 11:35:08 +1300

    Added HttpPost request to CreateUserController
    Validates fields in user are not null and returns BadRequest if needed
    
    returns user in OK response

--- CreateUserTestAPI/Controllers/CreateUserController.cs
+++ CreateUserTestAPI/Controllers/CreateUserController.cs
@@ -7,9 +7,8 @@ namespace CreateUserTestAPI.Controllers
     [ApiController]
     public class CreateUserController : ControllerBase
     {
-
         // get method to return all users and test the api
-        // functionality not requested and could be removed in future
+        // functionality not requested so could be removed in future
         [HttpGet]
         public IEnumerable<User> GetUsers()
         {
@@ -19,6 +18,48 @@ namespace CreateUserTestAPI.Controllers
             };
         }
 
-        
+        // post method to create a new user
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<User> CreateUser([FromBody]User user)
+        {
+         // returns BadRequest for null user
+            if (user == null)
+            {
+                return BadRequest(user);
+            } 
+
+        // checks each of the fields in user and returns BadRequest with error message if one of them is null
+            // UPDATE: could be updated to check all fields and return appropriate error messages if multiple fields are missing
+            if (string.IsNullOrEmpty(user.FirstName))
+            {
+                return BadRequest("FirstName cannot be null");
+            }
+            if (string.IsNullOrEmpty(user.LastName)) 
+            {
+                return BadRequest("LastName cannot be null");
+            }
+
+            // UPDATE: is expecting a DateTime object in the request - could be updated to expect a string in a DTO but store a DateTime object in the User/DB
+            try
+            {
+                DateTime dateOfBirth = Convert.ToDateTime(user.DateOfBirth); 
+            } catch 
+            {
+                return BadRequest("Date of birth must be in the format dd/mm/yyyy");
+            }
+
+            if (string.IsNullOrEmpty(user.Address))
+            {
+                return BadRequest("Address cannot be null");
+            }
+
+            // UPDATE: add further validation 
+
+        // returns OK and the user if passess validation
+            // UPDATE: store user
+            return Ok(user);
+        }
     }
 }
