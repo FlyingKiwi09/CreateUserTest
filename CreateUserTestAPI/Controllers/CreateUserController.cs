cf7e48fa526f3b81205e054d489cc16ea4542ddf
FlyingKiwi09 <102623183+FlyingKiwi09@users.noreply.github.com>
2023-03-01 11:22:19 +1300

    Removed default controller and added CreateUserController
    Set up User Model  and CreateUserController with a route and HttpGet method. Returns users.

---
+++ CreateUserTestAPI/Controllers/CreateUserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CreateUserTestAPI.Model;
+
+namespace CreateUserTestAPI.Controllers
+{
+    [Route("api/CreateUserTestAPI")]
+    [ApiController]
+    public class CreateUserController : ControllerBase
+    {
+
+        // get method to return all users and test the api
+        // functionality not requested and could be removed in future
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            return new List<User>
+            {
+                new User{FirstName="Bob", LastName="Jones", DateOfBirth=new DateTime(22/09/1989), Address="4 Road, Raumati"}
+            };
+        }
+
+        
+    }
+}
---
+++ CreateUserTestAPI/Model/User.cs
@@ -0,0 +1,10 @@
+﻿namespace CreateUserTestAPI.Model
+{
+    public class User
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public String Address { get; set; }
+    }
+}
